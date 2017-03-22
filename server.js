bfd5b9efd855f6d8d8662a6351f0e55d57a1381b
System <none>
2017-03-22 16:35:19 +0000

    header extraction functions done

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 "use strict";
 
 const express = require('express');
+const getHeaders = require('./services/headers');
 
 const server = express();
 
 server.get('/', (req, res) => {
+  const json = getHeaders(req.headers);
+  
+  console.log(json);
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(req.headers));
 });
